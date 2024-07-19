73278f89b442ec3cf7286070ef5944d991f9daad
Shikhare Shivam <shikhareshivam123@gmail.com>
2024-07-20 01:43:48 +0530

    Added files to learn the window events

---
+++ windowEventsScript.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        $("body").css("background-color", "yellow")
+    });
+
+    $("#box").scroll(function () {
+        $(this).css("color", "white")
+    });
+
+    $(window).resize(function () {
+        $("body").css("background-color", "")
+    });
+});
